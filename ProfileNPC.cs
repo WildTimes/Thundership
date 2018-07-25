f17c06fe5520ce5c928529e42b1e6e3eb511b9d0
WildTimes <41583422+WildTimes@users.noreply.github.com>
2018-07-25 12:39:15 -0300

    Initial commit

---
+++ ProfileNPC.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfileNPC : ScriptableObject
+{
+
+    [HideInInspector] public string name;
+    [HideInInspector] public int age;
+    [HideInInspector] public bool isMale;
+    [HideInInspector] public string resume;
+    [HideInInspector] public Sprite[] pictures;
+    private int affinity;
+}
